af267de2400ae90a569500f643c942d7e6cda556
Tadeh Boghozian <boghoziant@gmail.com>
2022-08-05 19:42:58 -0700

    Implemented mini-max algorithm and a/b pruning
    Implementing mini-max algorithm caused the ai to look more steps ahead
    and also updated the design document to reflect the changes. Also used
    alpha beta pruning to reduce the number of positions to be looked at

--- script.js
+++ script.js
@@ -14,35 +14,12 @@ function makeRandomMove () {
 }
 
 function makeEvalMove () {
-    var currentBoard = board.fen().split('')
-
     // get current score of board
     // go through fen string 
-    var total = 0
+    var total = eval(game)
     
-    
-    for (let i = 0; i < currentBoard.length; i++) {
-        var piece = currentBoard[i]
-        // Skip '/' in fen
-        if (!piece.match(/[a-z]/i)) {
-            continue
-        }
-
-        // If uppercase then it's a white piece which means
-        // the piece value should be added to the score 
-        var white = 1
-        if (piece == piece.toLowerCase()) {
-            white = -1
-        }
-
-        piece = piece.toLowerCase()
-        total += pieceValue(piece, white)
-    }
-
     var possibleMoves = game.moves({verbose : true})
     if (possibleMoves.length == 0) return
-
-    
     
     var minTotal = total
     var bestMove = possibleMoves[0]
@@ -70,8 +47,119 @@ function makeEvalMove () {
     board.position(game.fen())
 }
 
-function pieceValue (p, white) {
+function eval(b) {
+    var currentBoard = b.fen().split('')
 
+    // get current score of board
+    // go through fen string 
+    var total = 0
+    
+    for (let i = 0; i < currentBoard.length; i++) {
+        var piece = currentBoard[i]
+        // Skip '/' in fen
+        if (piece == ' ') break
+        if (!piece.match(/[a-z]/i)) {
+            continue
+        }
+
+        // If uppercase then it's a white piece which means
+        // the piece value should be added to the score 
+        var white = 1
+        if (piece == piece.toLowerCase()) {
+            white = -1
+        }
+
+        piece = piece.toLowerCase()
+        total += pieceValue(piece, white)
+    }
+    return total
+}
+
+function topLevelMiniMax() {
+    var res = miniMax(game, 2, false, -10000, 10000)
+
+    game.move(res.m)
+    board.position(game.fen())
+}
+
+function shuffle(array) {
+  let currentIndex = array.length,  randomIndex;
+
+  // While there remain elements to shuffle.
+  while (currentIndex != 0) {
+
+    // Pick a remaining element.
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex--;
+
+    // And swap it with the current element.
+    [array[currentIndex], array[randomIndex]] = [
+      array[randomIndex], array[currentIndex]];
+  }
+
+  return array;
+}
+
+function miniMax(node, depth, maximizingPlayer, move=null, alpha, beta) {
+    if (depth == 0) {
+        var bestMove = {
+            v: eval(node),
+            m: move
+        }
+        return bestMove
+    }
+    if (maximizingPlayer) {
+        var children = shuffle(node.moves())
+        length = children.length
+        var child = new Chess()
+        var bestMove = {
+            v: -10000,
+            m: move
+        }
+
+        for (var i = 0; i < length; i++) {
+            child.load(node.fen())
+            child.move(children[i])
+            value = miniMax(child, depth - 1, false, children[i], alpha, beta)
+            if (value.v > bestMove.v) {
+                bestMove.v = value.v
+                bestMove.m = children[i]
+            }
+            alpha = Math.max(alpha, bestMove.v);
+            if (beta <= alpha) {
+                return bestMove;
+            }
+        }
+
+        return bestMove
+    } else /* minimizingPlayer */{
+        var children = shuffle(node.moves())
+        length = children.length
+        var child = new Chess()
+        bestMove = {
+            v: 10000,
+            m:children[0] 
+        }
+
+        for (var i = 0; i < length; i++) {
+            child.load(node.fen())
+            child.move(children[i])
+            value = miniMax(child, depth - 1, true, children[i], alpha, beta)
+            if (value.v < bestMove.v) {
+                bestMove.v = value.v
+                bestMove.m = children[i]
+            }
+            beta = Math.min(beta, bestMove.v);
+            if (beta <= alpha) {
+                return bestMove;
+            }
+        }
+        return bestMove
+    }
+}
+
+
+function pieceValue (p, white) {
       switch(p) {
                 case 'p':
                     return  10 * white
@@ -112,7 +200,7 @@ function onDrop(source, target) {
     if (move == null) return 'snapback'
 
     // window.setTimeout(makeRandomMove, 550)
-    window.setTimeout(makeEvalMove, 550)
+    window.setTimeout(topLevelMiniMax, 50)
 }
 
 function onSnapEnd() {
@@ -122,7 +210,7 @@ function onSnapEnd() {
 
 var config = {
     draggable: true,
-    position: 'start',
+    position: game.fen(),
     onDragStart: onDragStart,
     onDrop: onDrop,
     onSnapEnd: onSnapEnd
